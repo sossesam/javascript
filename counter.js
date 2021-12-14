521eb4ab5633ca780ca8e449a21085f2a2cd1e7c
sossesam <sossesam@gmail.com>
2021-12-14 15:21:43 +0100

    dfdfdfdf

--- counter.js
+++ counter.js
@@ -10,20 +10,20 @@ colorsArray = ["red","blue","pink","green","purple","yellow"];
 
     
     if(5 <= t && 12 > t){
-    document.getElementById("telltime").innerHTML= "good"+ " "+moment[0] + t;
+    document.getElementById("telltime").innerHTML= "good"+ " "+moment[0] ;
    document.getElementById("image").src = "https://d1csarkz8obe9u.cloudfront.net/posterpreviews/12-good-morning-design-template-0e63204000164c6678e2e5f41b12ecc6_screen.jpg?ts=1597682115";
     
 }
 else if (12 <= t && 17 > t) {
-    document.getElementById("telltime").innerHTML= "good"+ " "+moment[1] + t;
+    document.getElementById("telltime").innerHTML= "good"+ " "+moment[1];
     document.getElementById("image").src = "https://www.yourselfquotes.com/wp-content/uploads/2017/10/Good-Afternoon-Quotes-and-Sayings.jpg";
     
 }else if (17 <= t && 20 > t) {
-    document.getElementById("telltime").innerHTML= "good"+ " "+moment[2] + t;
+    document.getElementById("telltime").innerHTML= "good"+ " "+moment[2] ;
     document.getElementById("image").src = "https://i.pinimg.com/736x/87/01/84/870184100e94be5fb52a0a6ba84f3017.jpg";
     
 }else if (20 <= t && 24 > t) {
-    document.getElementById("telltime").innerHTML= "good"+ " "+moment[3] + t;
+    document.getElementById("telltime").innerHTML= "good"+ " "+moment[3];
     document.getElementById("image").src = "https://api.time.com/wp-content/uploads/2017/03/time-health-stock-sleep-bed-night.jpg?quality=85&w=1024&h=512&crop=1";
     
 }
